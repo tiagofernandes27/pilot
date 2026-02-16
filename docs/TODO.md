b048fd14f439a350db159d422bcd80f33d5268f8
Tiago Fernandes <tiago.andre.fernandes77@gmail.com>
2026-02-16 18:55:43 +0000

    Character & Projectile

--- docs/TODO.md
+++ docs/TODO.md
@@ -2,9 +2,12 @@
 
 ### Player
 (X) Create player node\
-( ) Implement player movement\
-( ) Make player always face cursor\
+(X) Implement player movement\
+(X) Make player always face cursor\
+( ) Make projectile node\
+( ) Make player shoot\
 
 ### Visuals (Not necessary atm, but cool to keep the motivation :p )
 ( ) Pilot asset\
 ( ) Cursor asset\
+( ) Projectile asset\
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
+@export var move_speed = 300.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+func _ready():
+	pass
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	move()
+	rotate(get_angle_to(get_global_mouse_position()) + deg_to_rad(90.0))
 	move_and_slide()
+
+func move():
+	var direction : Vector2
+	direction.x = Input.get_axis("left", "right")
+	direction.y = Input.get_axis("up", "down")
+	direction = direction.normalized()
+	if direction:
+		velocity = direction * move_speed
+	else:
+		velocity = Vector2.ZERO
